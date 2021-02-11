78451d5d3b7e27101b26938e43e4018813eab63a
David Flood <davidfloodii@gmail.com>
2021-02-11 11:47:09 +0000

    add underdot unicode 0323 to ignore_unclear

--- collation/core/default_implementations.py
+++ collation/core/default_implementations.py
@@ -4,7 +4,9 @@ class RuleConditions(object):
 
     def ignore_unclear(self, decision_word, token_words):
         decision_word = decision_word.replace('_', '')
+        decision_word = decision_word.replace('\u0323', '')
         token_words = [w.replace('_', '') for w in token_words]
+        token_words = [w.replace('\u0323', '') for w in token_words]
         return(decision_word, token_words)
 
     def ignore_supplied(self, decision_word, token_words):
@@ -25,4 +27,5 @@ class ApplySettings(object):
 
     def hide_unclear_text(self, token):
         token['interface'] = token['interface'].replace('_', '')
+        token['interface'] = token['interface'].replace('\u0323', '')
         return token
